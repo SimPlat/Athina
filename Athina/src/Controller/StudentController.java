403d171d9760e35039fde6d5346f2407aff760ba
it174 <it174@Stratos.lan>
2020-11-22 18:46:43 +0200

    Controllers for MVC

---
+++ Athina/src/Controller/StudentController.java
@@ -0,0 +1,12 @@
+/*
+The Student controller contains all interactions of Student type Users that connect with the database
+ */
+package Controller;
+import Model.Student;
+/**
+ *
+ * @author it174
+ */
+public class StudentController {
+    Student std;
+}
