5593c03884b605c3340aafba0d5cf1903db515c7
ChristosKounatidis <41286370+ChristosKounatidis@users.noreply.github.com>
2020-11-26 22:00:45 +0200

    Update StudentController.java

--- src/main/java/athina/controller/StudentController.java
+++ src/main/java/athina/controller/StudentController.java
@@ -2,11 +2,24 @@
 The Student controller contains all interactions of Student type Users that connect with the database
  */
 package controller;
+import model.Lecture;
 import model.Student;
-/**
- *
- * @author it174
- */
+
 public class StudentController {
     Student std;
+
+    //us005
+    public void showAvailableLectures(){
+        //
+    }
+
+    public void onClickUploadEnrollments(Lecture lectures[]){
+        //
+    }
+    
+    //us006
+    public void onClickUploadEditedEnrollment(Lecture lectures[]){
+        //
+    }
+
 }
