8762d0bb5030057578b04c9f1efe41d5e59dcebd
ChristosKounatidis <41286370+ChristosKounatidis@users.noreply.github.com>
2020-11-28 19:58:53 +0200

    Update SecretaryController.java

--- src/main/java/athina/controller/SecretaryController.java
+++ src/main/java/athina/controller/SecretaryController.java
@@ -2,11 +2,11 @@
 The SecrataryController contains all interactions of Secretary type Users that connect with the database
  */
 package athina.controller;
-import athina.model.Secratary;
+import athina.model.Secretary;
 import athina.model.Student;
 
 public class SecretaryController {
-    Secratary secr;
+    Secretary secr;
 
     //us011
     public void onClickEventUploadStudent(String info){
